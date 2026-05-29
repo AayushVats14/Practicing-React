23ac14b7bd628b401ec81a6fc970e43601b5d85b
Aayush Vats <aayushvats018@gmail.com>
2026-05-29 13:20:49 +0530

    Day 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import Change from "./hooks.jsx";
 import Counter from "./Counter.jsx";
 import Display from "./Display.jsx";
 import User2 from "./User2.jsx";
+import { useState } from "react";
+import Student from "./Student.jsx";
+import Default from "./Default.jsx";
+import Wrapper from "./Wrapper.jsx";
 
 function App() {
+  const [student, setStudent] = useState();
+
   return (
     <>
       <ToDo />
@@ -15,6 +21,22 @@ function App() {
       <Change />
       <Counter />
       <h2>Props in React:</h2>
+      {student && <Student name={student} />}
+      <button onClick={() => setStudent("Patrick")}>Update Student</button>
+      <Default name="Peter" />
+      <Default />
+      <Wrapper color="Orange">
+        <h1>Hello World</h1>
+      </Wrapper>
+
+      <Wrapper>
+        <h2>Hello Kai</h2>
+      </Wrapper>
+
+      <Wrapper>
+        <h3>Hello admin</h3>
+        <h4 style={{ color: "Red" }}>Login to continue</h4>
+      </Wrapper>
     </>
   );
 }
---
+++ src/Default.jsx
@@ -0,0 +1,5 @@
+function Default({ name = "New User" }) {
+  return <h2>Hi {name}</h2>;
+}
+
+export default Default;
